9d7c3add3ec09dd1ff121385d349a16c27a9246d
pratik10012001 <pratikasbe10@gmail.com>
2025-09-26 23:58:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,95 @@
 # Create Kubernetes namespaces
+
 resource "kubernetes_namespace" "demo" {
+
   metadata {
+
     name = "demo"
+
   }
+
 }
  
 resource "kubernetes_namespace" "openobserve" {
+
   metadata {
+
     name = "openobserve"
+
   }
+
 }
  
-# Deploy nginx
+# Locals: split YAML docs in case file has multiple documents (---)
+
+locals {
+
+  nginx_docs  = [for doc in split("---", file("${path.module}/../k8s/nginx-deploy.yaml")) : yamldecode(trimspace(doc)) if trimspace(doc) != ""]
+
+  java_docs   = [for doc in split("---", file("${path.module}/../k8s/my-java-app-deployment.yaml")) : yamldecode(trimspace(doc)) if trimspace(doc) != ""]
+
+  logger_docs = [for doc in split("---", file("${path.module}/../k8s/logger-app.yaml")) : yamldecode(trimspace(doc)) if trimspace(doc) != ""]
+
+}
+ 
+# Deploy nginx (all docs inside nginx-deploy.yaml)
+
 resource "kubernetes_manifest" "nginx" {
-  manifest   = yamldecode(file("${path.module}/../k8s/nginx-deploy.yaml"))
+
+  for_each   = { for i, v in local.nginx_docs : i => v }
+
+  manifest   = each.value
+
   depends_on = [kubernetes_namespace.demo]
+
 }
  
-# Deploy java app
+# Deploy java app (all docs inside my-java-app-deployment.yaml)
+
 resource "kubernetes_manifest" "java" {
-  manifest   = yamldecode(file("${path.module}/../k8s/my-java-app-deployment.yaml"))
+
+  for_each   = { for i, v in local.java_docs : i => v }
+
+  manifest   = each.value
+
   depends_on = [kubernetes_namespace.demo]
+
 }
  
-# Deploy logger app
+# Deploy logger app (all docs inside logger-app.yaml)
+
 resource "kubernetes_manifest" "logger" {
-  manifest   = yamldecode(file("${path.module}/../k8s/logger-app.yaml"))
+
+  for_each   = { for i, v in local.logger_docs : i => v }
+
+  manifest   = each.value
+
   depends_on = [kubernetes_namespace.demo]
+
 }
  
 # Helm release for OpenObserve
+
 resource "helm_release" "openobserve" {
+
   name       = "openobserve-standalone"
+
   repository = "https://charts.openobserve.ai"
+
   chart      = "openobserve-standalone"
+
   namespace  = kubernetes_namespace.openobserve.metadata[0].name
+
   version    = var.openobserve_chart_version
  
   values = [
+
     file("${path.module}/${var.openobserve_values_file}")
+
   ]
  
   depends_on = [kubernetes_namespace.openobserve]
+
 }
+
+ 
